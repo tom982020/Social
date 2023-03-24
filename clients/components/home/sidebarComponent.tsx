6f6fb2d515b21ca51522e0b12292934ec2617120
Tom <bastom20@gmail.com>
2023-03-24 17:31:45 +0700

    add new sidebar

--- clients/components/home/sidebarComponent.tsx
+++ clients/components/home/sidebarComponent.tsx
@@ -1,11 +1,46 @@
 /** @format */
 'use client';
 
-import { Aside, MediaQuery, Text, useMantineTheme } from '@mantine/core';
+import { Aside, MediaQuery, NavLink, Text, useMantineTheme } from '@mantine/core';
+import { dataUserNav } from 'constant/data/navbar';
+import { useState } from 'react';
 
 const SidebarComponent = () => {
 	const theme = useMantineTheme();
-
+	const [active, setActive] = useState(0);
+	const [activeChild, setActiveChild] = useState(0);
+	
+	const itemsUser = dataUserNav.map((item, index) => {
+		return (
+			<NavLink
+				key={item.label}
+				active={index === active}
+				label={item.label}
+				icon={
+					<item.icon
+						size="1rem"
+						stroke={1.5}
+					/>
+				}
+				childrenOffset="xl"
+				variant="filled"
+				style={{
+					fontSize: '20px',
+				}}
+				onClick={() => setActive(index)}>
+				{item.child?.map((child, idx) => (
+					<NavLink
+						key={child.label}
+						label={child.label}
+						active={idx === activeChild}
+						onClick={() => {
+							setActiveChild(idx);
+						}}
+					/>
+				))}
+			</NavLink>
+		);
+	});
 	return (
 		<MediaQuery
 			smallerThan="sm"
@@ -21,7 +56,7 @@ const SidebarComponent = () => {
 							? theme.colors.dark[8]
 							: theme.colors.gray[0],
 				}}>
-				<Text>Application sidebar</Text>
+				{itemsUser}
 			</Aside>
 		</MediaQuery>
 	);
