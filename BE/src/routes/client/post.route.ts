3cb85ffc3eb74dccd497459df6692a2ae79853ea
Tom <bastom20@gmail.com>
2023-04-03 13:59:05 +0700

    remove checktoken get post all

--- BE/src/routes/client/post.route.ts
+++ BE/src/routes/client/post.route.ts
@@ -4,12 +4,12 @@ import postController from '../../controller/client/post.controller';
 
 const postRoute = express.Router();
 
+postRoute.get('/all', postController.getPostAll);
 postRoute.use(checkToken);
 postRoute.post('/create', postController.creatPost);
 postRoute.post('/comment', postController.commentPost);
 postRoute.get('/post-user', postController.getPostForUser);
 postRoute.get('/comment-user', postController.getComments);
-postRoute.get('/all', postController.getPostAll);
 postRoute.put('/update-heart/:postID', postController.heartPost)
 
 
