7d83a99d852ac5b8f1f0657c8ac5a020c490393e
Tom <bastom20@gmail.com>
2023-04-04 13:49:34 +0700

    fix get page

--- BE/src/library/paginate.ts
+++ BE/src/library/paginate.ts
@@ -1,26 +1,41 @@
-import paginate from 'mongoose-paginate-v2';
-const paginateHandler = async (query: any, model: any, select: any, populate: any) => {
-    let options = {
-        query: {},
-        select: '',
-        sort: { createdAt: -1 },
-        lean: true,
-        populate: '',
-        page: 1,
-        limit: 10,
-    }
-    if (select != null) {
-        options.select = select
-    }
-    if (query != null) {
-        options.query = query
-    }
-    if (populate != null) {
-        options.populate = populate
-    }
+/** @format */
 
-    const results = await model.paginate(options)
-    return results
-}
+import paginate from 'mongoose-paginate-v2';
+const paginateHandler = async (
+	query: any,
+	model: any,
+	select: any,
+	populate: any
+) => {
+	let options = {
+		query: {},
+		select: '',
+		sort: { createdAt: -1 },
+		lean: true,
+		populate: '',
+		page: 1,
+		limit: 10,
+	};
+	if (select != null) {
+		options.select = select;
+	}
+	if (query != null) {
+		options.query = query;
+	}
+	if (populate != null) {
+		options.populate = populate;
+	}
+	if (query.limit) {
+		options.limit = query.limit;
+		delete query.limit;
+	}
+	if (query.page) {
+		options.page = query.page;
+		delete query.page;
+	}
+
+	const results = await model.paginate(options);
+	return results;
+};
 
 export default paginateHandler;
